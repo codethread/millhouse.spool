eadacb048044e5db5e6d9c68afa9b078ff78b831
codethread <adamhalldesigns@gmail.com>
2026-08-11 21:32:24 +0100

    fix: drop obsolete CodeThread spool-bump
    Align the Millhouse workspace with CodeThread 26fdf69, where the
    spool-bump root was removed and bump ownership moved to Millhouse.

--- .millstrand/init.clj
+++ .millstrand/init.clj
@@ -75,11 +75,6 @@
                   :after [:millstrand/spools-agent-run
                           :millstrand/spools-delegation]
                   :required? true})
-(runtime/module! runtime :codethread/spool-bump
-                 {:ns 'ct.spools.codethread.spool-bump
-                  :spools ['codethread/spool-bump 'millhouse.spools/workflow]
-                  :after [:millhouse/spools-workflow]
-                  :required? true})
 (runtime/module! runtime :codethread/devflow-setup
                  {:ns 'ct.spools.codethread.devflow-setup
                   :spools ['codethread/devflow-setup]
