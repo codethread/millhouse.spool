16ecb604fe1888046f2f88d844a85a671fe2b440
codethread <adamhalldesigns@gmail.com>
2026-08-30 19:25:30 +0100

    chore: erase spool cutover residue

--- spools/workflow/test/millhouse/spools/millstrand_workflows/bump_millstrand_test.clj
+++
@@ -1,8 +0,0 @@
-(ns millhouse.spools.millstrand-workflows.bump-millstrand-test
-  "Regression coverage for retiring the manifest-era Millstrand bump workflow."
-  (:require [clojure.java.io :as io]
-            [clojure.test :refer [deftest is]]))
-
-(deftest legacy-millstrand-bump-workflow-is-not-shipped
-  (is (nil? (io/resource
-             "millhouse/spools/millstrand_workflows/bump_millstrand.clj"))))
